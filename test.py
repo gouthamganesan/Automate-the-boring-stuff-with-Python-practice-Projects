c26f162076a3807be61cb8a8c138f11e2c244c8e
gouthamganesan <gouthamganesan98@gmail.com>
2017-12-21 21:19:31 +0530

    -

--- test.py
+++ test.py
@@ -6,7 +6,6 @@ for folderName, subfolders, filenames in os.walk('/home/xprodigy/Prog/'):
     for filename in filenames:
         print('FILE INSIDE ' + folderName + ': '+ filename)
     print('')
-'''
 import sys, tty, termios
 while True:
     fd = sys.stdin.fileno()
@@ -19,3 +18,23 @@ while True:
     print('Youve entered ', ch)
     if ch == '':
         break
+'''
+
+import logging
+logging.disable(logging.CRITICAL)
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+logging.debug('Start of program')
+
+
+def factorial(n):
+    logging.debug('Start of factorial({}%)'.format(n))
+    total = 1
+    for i in range(1, n + 1):
+        total *= i
+        logging.debug('i is ' + str(i) + ', total is ' + str(total))
+    logging.debug('End of factorial({}%)'.format(n))
+    return total
+
+
+print(factorial(5))
+logging.debug('End of program')
