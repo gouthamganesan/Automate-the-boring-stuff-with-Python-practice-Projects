a5d877da0762e8b52c490bc567e4c975883e373e
gouthamganesan <gouthamganesan98@gmail.com>
2017-12-23 13:44:53 +0530

    DebugCoinToss completed

--- debugCoinToss.py
+++ debugCoinToss.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python
+import random, sys
+guess = ''
+while guess not in ('heads', 'tails'):
+    print('Guess the coin toss! Enter heads or tails:')
+    guess = input()
+toss = random.randint(0, 1)
+if toss == 1:
+    toss = 'heads'
+else:
+    toss = 'tails'
+if toss == guess:
+    print('You got it!')
+    sys.exit()
+else:
+    print('Nope! Guess again!')
+guess = input()
+while guess not in ('heads', 'tails'):
+    print('Guess the coin toss! Enter heads or tails:')
+    guess = input()
+if toss == guess:
+    print('You got it!')
+else:
+    print('Nope. You are really bad at this game.')
